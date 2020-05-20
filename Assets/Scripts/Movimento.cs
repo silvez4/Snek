337a233f51fa4e14e014f41a1ae04bf54a87031d
silvez4 <gabriel.ieda@hotmail.com>
2020-05-20 14:33:01 -0300

    Movimento Aprimorado
    Impedimento de Movimentos contra regra
    E controle de sprites

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Movimento : MonoBehaviour
 {
+    [Header("Movimento")]
     public float vel;
     private Rigidbody2D rb;
     public Vector2 Dir;
     
+    [Header("Controle Sprites e Movimentação")]
+    public bool MovendoHoriz;
+    public bool MovendoVert;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Dir = Vector2.right;
+        MovendoHoriz = true;
     }
     private void Update() 
     {
@@ -22,16 +27,28 @@ public class Movimento : MonoBehaviour
         
     }
     private Vector2 Mover(float x, float y){
-        if(x>0){
+        if(x>0 && MovendoVert){
+            MovendoVert = false;
+            MovendoHoriz = true;
+            this.transform.rotation = Quaternion.Euler(0,0,0); 
             return Vector2.right;
         }
-        else if(x<0){
+        else if(x<0 && MovendoVert){
+            MovendoVert = false;
+            MovendoHoriz = true;
+            this.transform.rotation = Quaternion.Euler(0,0,180); 
             return Vector2.left;
         }
-        else if(y>0){
+        else if(y>0 && MovendoHoriz){
+            MovendoVert = true;
+            MovendoHoriz = false;
+            this.transform.rotation = Quaternion.Euler(0,0,90);
             return Vector2.up;
         }
-        else if(y<0){
+        else if(y<0 && MovendoHoriz){
+            MovendoVert = true;
+            MovendoHoriz = false;
+            this.transform.rotation = Quaternion.Euler(0,0,-90);
             return Vector2.down;
         }
         else{
