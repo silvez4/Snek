638b9fa013a7a2e3916620148d8caef76f779551
silvez4 <gabriel.ieda@hotmail.com>
2020-05-20 18:19:04 -0300

    Esquelo Montado
    -Movimento a base de grid
    -Frutas e mecanica de spaw e coleta Adicionadas
    -Borda dos mapas

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Movimento : MonoBehaviour
 {
     [Header("Movimento")]
-    public float vel;
     private Rigidbody2D rb;
-    public Vector2 Dir;
+    public Vector2Int gridPos;
+    private float tempomov;
+    private float tempomovMax;
+
     
     [Header("Controle Sprites e Movimentação")]
     public bool MovendoHoriz;
@@ -15,44 +17,61 @@ public class Movimento : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Dir = Vector2.right;
+        tempomovMax = .3f;
+        tempomov = tempomovMax;
         MovendoHoriz = true;
+        gridPos = Vector2Int.right;
+        transform.position += new Vector3 (0,0);
     }
     private void Update() 
     {
+        ControleMovimento();
+        ControleTempo();
+    }
+    private void ControleMovimento(){
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Dir = Mover(x,y);
-        rb.velocity = (Dir * vel);
-        
+        gridPos = Mover(x,y);
     }
-    private Vector2 Mover(float x, float y){
+    private void ControleTempo(){
+         tempomov += Time.deltaTime;
+        if(tempomov >= tempomovMax){
+            transform.position += new Vector3 (gridPos.x,gridPos.y);
+            tempomov -=tempomovMax;
+        }
+    }
+    private Vector2Int Mover(float x, float y){
         if(x>0 && MovendoVert){
             MovendoVert = false;
             MovendoHoriz = true;
             this.transform.rotation = Quaternion.Euler(0,0,0); 
-            return Vector2.right;
+            return Vector2Int.right;
         }
         else if(x<0 && MovendoVert){
             MovendoVert = false;
             MovendoHoriz = true;
             this.transform.rotation = Quaternion.Euler(0,0,180); 
-            return Vector2.left;
+            return Vector2Int.left;
         }
         else if(y>0 && MovendoHoriz){
             MovendoVert = true;
             MovendoHoriz = false;
             this.transform.rotation = Quaternion.Euler(0,0,90);
-            return Vector2.up;
+            return Vector2Int.up;
         }
         else if(y<0 && MovendoHoriz){
             MovendoVert = true;
             MovendoHoriz = false;
             this.transform.rotation = Quaternion.Euler(0,0,-90);
-            return Vector2.down;
+            return Vector2Int.down;
         }
         else{
-            return Dir;
+            return gridPos;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Wall"){
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/SpawnFrutas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFrutas : MonoBehaviour
+{
+    public GameObject[] Frutas;
+    private int maxGrid = 14;
+    private int minGrid = -14;
+    public float tempoSpaw;
+    public float tempoSpawMax = 3f;
+    public int qtdSpawnada;
+    void Start()
+    {
+        tempoSpaw=tempoSpawMax;
+        qtdSpawnada = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempoSpaw += Time.deltaTime;
+        if(tempoSpaw >= tempoSpawMax){
+            tempoSpaw -= tempoSpawMax;
+            if(qtdSpawnada <6){
+                Spawnar();
+            }
+        }
+    }
+    private void Spawnar(){
+        //Uma Fruta Random, Posição Random entre os limites do tabuleiro, sem rotação 
+        Instantiate(Frutas[Random.Range(0,Frutas.Length)],new Vector3Int(Random.Range(minGrid,maxGrid),Random.Range(minGrid,maxGrid),0),Quaternion.identity);
+        qtdSpawnada++;
+    }
+    public void PegouFruta(){
+        qtdSpawnada--;
+    }
+}
